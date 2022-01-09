bfa8f06708cedbf0ed3cec4c7f939593d1ef7f0e
DC Scott <dcscottwrites@gmail.com>
2022-01-09 14:09:15 -0800

    Torque/Player Control
    added torqueAmount variable to left arrow & a key, added -torqueAmount variable to right arrow & d key.

--- Snow Board Zen/Assets/Scripts/PlayerController.cs
+++ Snow Board Zen/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    Rigidbody2D rb2d;
+    [SerializeField] float torqueAmount = 1f;
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            rb2d.AddTorque(torqueAmount);
+        }        
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb2d.AddTorque(-torqueAmount);
+        }     
     }
 }
