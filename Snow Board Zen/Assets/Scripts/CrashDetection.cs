c3b60225ae64ea5f83a967699ddacfbcb038bc0c
DC Scott <dcscottwrites@gmail.com>
2022-01-10 12:42:20 -0800

    Crash Detection Added

--- Snow Board Zen/Assets/Scripts/CrashDetection.cs
+++ Snow Board Zen/Assets/Scripts/CrashDetection.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetection : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class CrashDetection : MonoBehaviour
     {
         if(other.tag == "Ground")
         {
+            SceneManager.LoadScene(0);
             Debug.Log("You have crashed! Please start from the beginning...");
         }    
     }
